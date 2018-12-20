2f19e2adc8fe514d46cc8cb6db8122143a56e6a7
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2018-12-21 01:23:44 +0530

    Add setter methods

--- knowledge_extracter.py
+++ knowledge_extracter.py
@@ -9,15 +9,33 @@ class KnowledgeExtractor:
 			self.text = text
 			self.extract_knowledge()
 
+	def set_text(self, text):
+		self.text = text
+
+	def set_text_and_extract(self, text):
+		self.text = text
+		self.extract_knowledge()
+
 	def extract_entities(self):
 		entity_data = self.comprehend.detect_entities(Text=self.text, LanguageCode='en')
 		return entity_data
 
 	def extract_keywords(self):
-		keyword_data = comprehend.detect_key_phrases(Text=text, LanguageCode='en')
+		keyword_data = comprehend.detect_key_phrases(Text=self.text, LanguageCode='en')
 		return keyword_data
 
 	def determine_sentiment(self):
-		sentiment_data = comprehend.detect_sentiment(Text=text, LanguageCode='en')
+		sentiment_data = comprehend.detect_sentiment(Text=self.text, LanguageCode='en')
 		return sentiment_data
-		
+
+	def extract_knowledge(self):
+		self.response = {
+			"entity_data": self.extract_entities,
+			"keyword_data": self.extract_keywords,
+			"sentiment_data": self.determine_sentiment
+		}
+
+		return self.response
+
+if __name__ == "__main__":
+	pass
