2e9e70a4ff6615886e2fbedf3f5b206e8a6c1d93
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2018-12-25 15:02:05 +0530

    Add similarity pair method

--- csim.py
+++ csim.py
@@ -4,11 +4,11 @@ class Csim:
 	def __init__(self):
 		self.slib = SimLib()
 
-	def find_sim_between_two(doc1, doc2):
+	def find_sim_between_two(self, doc1, doc2):
 		self.slib.set_data_and_find_sim(doc1, doc2)
 		return self.slib.get_response()
 
-	def find_similar_docs(primary_doc, list_of_docs, threshold=0.5):
+	def find_similar_docs(self, primary_doc, list_of_docs, threshold=0.5):
 		similar_docs = []
 
 		for doc in list_of_docs:
@@ -20,3 +20,18 @@ class Csim:
 				})
 
 		return similar_docs
+
+	def find_similarity_pairs(self, list_of_docs, threshold=0.5):
+		response = []
+
+		for index_i in range(len(list_of_docs)):
+			for index_j in range(index_i + 1, len(list_of_docs)):
+				doc1 = list_of_docs[index_i]
+				doc2 = list_of_docs[index_j]
+				score = self.find_sim_between_two(doc1.get("content"), doc2.get("content"))
+				response.append({
+					"docs": [doc1.get("name"), doc2.get("name")],
+					"score": score
+				})
+
+		return response
