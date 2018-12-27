da2d8676a6b96417a4b35585638ad803fb9fbc19
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2018-12-27 09:01:19 +0530

    Update util module

--- main.py
+++ main.py
@@ -40,6 +40,8 @@ def find_similar_docs():
 
 	
 
+
+
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
 
--- util.py
+++ util.py
@@ -3,4 +3,22 @@
 '''
 
 def is_valid_doc(doc):
-	pass
+	if doc.get("name") and doc.get("content"):
+		return True
+	return False
+
+def is_valid_list(docs):
+	for doc in docs:
+		if doc.get("name") and doc.get("content"):
+			continue
+		else:
+			return False
+	return False
+
+def build_response(status, data):
+	response = {
+		"status": status,
+		"data": data
+	}
+
+	return response
