dee04a808f9dd019bc20c1e6c56d67f3afdcc263
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2018-12-29 22:26:20 +0530

    Update lambda handler method

--- lambda_handler.py
+++ lambda_handler.py
@@ -6,10 +6,75 @@ import json
 from csim import Csim
 from util import *
 
+def find_sim_between_two(doc1, doc2):
+	if doc1 and doc2:
+		score = sim_handler.find_sim_between_two(doc1, doc2)
+		response = {
+			"status": "OK",
+			"data": {
+				"score": score
+			}
+		}
+	else:
+		response = {
+			"status": "FAILED",
+			"data": "NOT ENOUGH PARAMETERS"
+		}
 
+	return response
+
+def find_similar_docs(primary_doc, list_of_docs, threshold=None):
+	if not is_valid_doc(primary_doc):
+		return build_response("FAILED", "INVALID PRIMARY DOC")
+	if not is_valid_list(list_of_docs):
+		return build_response("FAILED", "INVALID LIST OF DOCS")
+	if threshold and not is_valid_threshold(threshold):
+		return build_response("FAILED", "INVALID THRESHOLD")
+
+	data = ""
+	if threshold:
+		data = sim_handler.find_similar_docs(primary_doc, list_of_docs, threshold)
+	else:
+		data = sim_handler.find_similar_docs(primary_doc, list_of_docs)
+
+	return build_response("OK", data)
+
+def find_similar_pairs(list_of_docs):
+	if not is_valid_list(list_of_docs):
+		return build_response("FAILED", "INVALID LIST OF DOCS")
+
+	data = sim_handler.find_similarity_pairs(list_of_docs)
+
+	return build_response("OK", data)
+
+def cluster_docs(list_of_docs, threshold=None):
+	if not is_valid_list(list_of_docs):
+		return build_response("FAILED", "INVALID LIST OF DOCS")
+	if threshold and not is_valid_threshold(threshold):
+		return build_response("FAILED", "INVALID THRESHOLD")
+
+	data = ""
+	if threshold:
+		data = sim_handler.cluster_docs(list_of_docs, threshold)
+	else:
+		data = sim_handler.cluster_docs(list_of_docs)
+
+	return build_response("OK", data)
 
 def handler(event, context):
 	operation = event.get("operation")
 
 	if not is_valid_op(operation):
 		return build_response("FAILED", "INVALID_OPERATION")
+
+	if operation == "find_sim_between_two":
+		return find_sim_between_two(event.get("doc1"), event.get("doc2"))
+
+	if operation == "find_similar_docs":
+		return find_similar_docs(event.get("primary_doc"), event.get("list_of_docs"), event.get("threshold"))
+
+	if operation == "find_similar_pairs":
+		return find_similar_pairs(event.get("list_of_docs"))
+
+	if operation == "cluster_docs":
+		return cluster_docs(event.get("list_of_docs"), event.get("threshold"))
