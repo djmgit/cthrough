5cfd8e9776068e7313bea29a6945c582d2e89d65
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-01-01 10:17:59 +0530

    updated packdep.sh

--- packdep.sh
+++ packdep.sh
@@ -13,3 +13,11 @@ cd $VIRTUALENV/lib/$PYTHON_VERSION/site-packages/
 zip -r9 ../../../../$ZIP_FILE *
 cd ../../../../
 zip -g $ZIP_FILE csim.py knowledge_extractor.py simlib.py stopwords.py lambda_func.py util.py
+
+echo "deploy to lambda"
+python3 update_lambda.py
+if [ $? = '0' ]; then
+	echo 'successfully deployed to lambda'
+else
+	echo 'deployment failed'
+fi
